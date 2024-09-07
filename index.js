654d2a048ead16c813902560f0496b7df3d814f7
GamerCleanVic <jonassd2000@gmail.com>
2024-09-07 12:37:49 -0300

    function's type

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-//ES5
-var pet = {
-    name: 'toninho',
-    specie: 'hamster' ,
-    age: 1.6
-};
+//function
 
-var age = pet.age;
+function soma1(num1, num2){
+    return num1 + num2;
+}
 
-//Destructing
-var { age2 } = pet;
+var soma2 = function(num1, num2){
+    return num1 + num2;
+}
 
-console.log(age2);
+const soma3 = (num1, num2) => {
+    return num1 + num2;
+}
+
+const soma4 = (num1, num2) => num1 + num2;
+
+const array = [1, 2, 3, 4 ,5].map((num1)=> num1*10);
+
+console.log(array);
